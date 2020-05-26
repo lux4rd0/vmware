c07e8b3e907705525888b478c9787fa74df5d720
Dave Schmid <dave@pulpfree.org>
2020-05-26 15:58:10 -0500

    Create discover_vms.sh

---
+++ discover_vms.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+host=$(hostname -s)
+
+list_of_vmids=$(vim-cmd vmsvc/getallvms | sed '1d' | awk '{if ($1 > 0) print $1}')
+
+if [ -n "${list_of_vmids}" ]; then
+
+echo "$list_of_vmids" | while IFS= read -r line ; do
+
+vm_name=$(vim-cmd vmsvc/get.summary "$line" |grep hostName |awk '{print $3}' | sed 's/[""]//g' | sed 's/[,]//g')
+vm_cpu=$(vim-cmd vmsvc/get.summary "$line" |grep numCpu |awk '{print $3}' | sed 's/[""]//g' | sed 's/[,]//g')
+vm_memory=$(vim-cmd vmsvc/get.summary "$line" |grep memorySizeMB |awk '{print $3}' | sed 's/[""]//g' | sed 's/[,]//g' |awk '{print $1/(1024)}')
+vm_storage=$(vim-cmd vmsvc/device.getdevices "$line" | grep capacityInKB | awk '{print $3}' | sed 's/[,]//g' |awk '{print $1/(1024*1024)}')
+
+echo "${host},${vm_name},${vm_cpu},${vm_memory},${vm_storage}"
+
+done
+
+fi
