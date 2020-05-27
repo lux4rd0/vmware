31d4424f20b554ff416fc93b3eb0213d7ed6d502
Dave Schmid <dave@pulpfree.org>
2020-05-26 20:10:46 -0500

    Update and rename discover_vms.sh to discover_vm.sh

--- discover_vm.sh
+++ discover_vm.sh
@@ -9,11 +9,12 @@ if [ -n "${list_of_vmids}" ]; then
 echo "$list_of_vmids" | while IFS= read -r line ; do
 
 vm_name=$(vim-cmd vmsvc/get.summary "$line" |grep hostName |awk '{print $3}' | sed 's/[""]//g' | sed 's/[,]//g')
+vm_ipaddress=$(nslookup ${vm_name} 2> /dev/null |grep Address: |tail -n 1 |awk '{print $2}')
 vm_cpu=$(vim-cmd vmsvc/get.summary "$line" |grep numCpu |awk '{print $3}' | sed 's/[""]//g' | sed 's/[,]//g')
 vm_memory=$(vim-cmd vmsvc/get.summary "$line" |grep memorySizeMB |awk '{print $3}' | sed 's/[""]//g' | sed 's/[,]//g' |awk '{print $1/(1024)}')
 vm_storage=$(vim-cmd vmsvc/device.getdevices "$line" | grep capacityInKB | awk '{print $3}' | sed 's/[,]//g' |awk '{print $1/(1024*1024)}')
 
-echo "${host},${vm_name},${vm_cpu},${vm_memory},${vm_storage}"
+echo "${host},${vm_name},${vm_ipaddress},${vm_cpu},${vm_memory},${vm_storage}"
 
 done
 
